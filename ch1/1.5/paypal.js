65316180afe5b4bed63f8221dfad5dc708f77d99
nichanank <n.kesonpat@gmail.com>
2019-11-24 22:59:44 +0700

    completed 1.5

--- ch1/1.5/paypal.js
+++ ch1/1.5/paypal.js
@@ -21,27 +21,30 @@ class Paypal extends Client {
     // the history of transactions
     this.txHistory = [];
 		// the network fee
-		// TODO
+		this.networkFee = 1;
 		// blacklist of accounts to be censored
-		// TODO
+		this.blacklist = [];
   }
 
 	// Removes funds from user accounts and adds them to Paypal's balance
 	// - In reality, it would be far easier for Paypal to mint themselves extra cash so they could look like a legit operation on the outside, but really just print money for themselves whenever they wanted. Then they would never be accused of stealing, but they could steal as much as they wanted. Yay magic internet money!
   stealAllFunds() {
 		// sums up all the value in the network
-		// TODO
-				// removes everyone's balance
-				// TODO
-		// adds that value to Paypal's wallet
-		// TODO
+		let totalFunds = 0;
+		for (const address in this.state) {
+			if (this.state[address] !== this.wallet.address) {
+				totalFunds += this.state[address].balance;
+				this.state[address].balance = 0;
+			}
+		}
+		this.state[this.wallet.address].balance += totalFunds;
   }
 
 	// Mints funds without adding the transaction to the history
 	// - In reality, this would not work as corporations have to get audited for tax purposes and whatnot, so they'd have to call this something clever like a network upgrade to invest in R&D or a "restructuring". In either case, there would be a glossy marketing campaign put together to make you feel good about it :)
 	mintSecretFunds(amount) {
 		// it's literally just 1 line of code...
-		// TODO
+		this.state[this.wallet.address].balance += amount;
 	}
 
   // Checks that the sender of a transaction is the same as the signer
@@ -61,15 +64,17 @@ class Paypal extends Client {
   // Check if the user's address is on a blacklist. If not, check is the user's address is already in the state, and if not, add the user's address to the state
   checkUserAddress(tx) {
 		// check if the sender or receiver are on the blacklist
-		// TODO
-			// if the sender or receiver are banned, return false
-			// TODO
+		// if the sender or receiver are banned, return false
+		if (this.blacklist.includes(tx.contents.from) || this.blacklist.includes(tx.contents.to)) {
+			console.log('blacklisted address')
+			return false;
+		}
     // check if the sender is in the state
     if (!(tx.contents.to in this.state)) {
 			// if the sender is not in the state, add their address and initialize an empty balance and nonce of 0
       this.state[tx.contents.to] = {
 				balance: 0,
-				nonce: 0
+				nonce: 0,
       };
     }
     // check if the receiver is in the state
@@ -77,7 +82,7 @@ class Paypal extends Client {
 			// if the receiver is not in the state, add their address and initialize an empty balance and nonce of 0
       this.state[tx.contents.from] = {
 				balance: 0,
-				nonce: 0
+				nonce: 0,
       };
     }
     // if the checks on both accounts pass (they're both in the state), return true
@@ -172,11 +177,11 @@ class Paypal extends Client {
 				}
 			}
 			// add the cancellation transaction to the txHistory
-			this.txHistory.push(tx)
+			this.txHistory.push(tx);
 			// charge a fee to the user for cancelling a transaction
 			this.state[tx.contents.from].balance -= 1;
 			// add that fee to Paypal's wallet balance
-			const paypalWallet = this.wallet.address
+			const paypalWallet = this.wallet.address;
 			this.state[paypalWallet].balance += 1;
 		}
   }
@@ -238,22 +243,24 @@ class Paypal extends Client {
 	// Charges a fee to use the network
 	chargeFee(tx) {
 		// removes the network fee from the sender's balance
-		// TODO
+		this.state[tx.contents.from].balance -= this.networkFee;
 		// adds the network fee to payment operator's balance
-		// TODO
+		this.state[this.wallet.address].balance += this.networkFee;
 	}
 
 	// Checks if a transaction is valid, then processes it, then checks if there are any valid transactions in the pending transaction pool and processes those too
 	processTx(tx) {
 		// charge a fee to use the network
-		// TODO
+		this.chargeFee(tx);
 		// check the transaction is valid
 		if (this.checkTx(tx)) {
 			// apply the transaction to Paypal's state
 			this.applyTransaction(tx);
 			// check if any pending transactions are now valid, and if so process them too
 			this.processPendingTx()
+			return true;
 		}
+		return false;
 	}
 }
 
