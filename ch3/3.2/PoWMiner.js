a48f16ca641351ba1e6fdb18d1a810368b23ef1c
nichanank <n.kesonpat@gmail.com>
2019-11-29 00:06:17 +0700

    completed 3.2

--- ch3/3.2/PoWMiner.js
+++ ch3/3.2/PoWMiner.js
@@ -15,37 +15,60 @@ class Miner extends Client {
   // Create a new block
   createBlock() {
     // get all the transactions for this block
-    // get the timestamp
+    const txList = [];
+    const tx = this.transactions.shift();
+    if (tx) txList.push(tx);
     // create a new block
-    // - nonce
-    // - block number
-    // - give ourselves the coinbase reward for finding the block
-    // - log the difficulty of the network
-    // - show the hash of the block that matches the network difficulty
-    // - timestamp
-    // - transactions
-    // return the block
+    const newBlock = {
+      nonce: 0, // nonce
+      number: this.blockNumber + 1, // block number
+      coinbase: this.wallet.address, // give ourselves the coinbase reward for finding the block
+      difficulty: this.difficulty, // log the difficulty of the network
+      parentHash: this.blockchain.slice(-1)[0], // show the hash of the block that matches the network difficulty
+      timestamp: Math.round(new Date().getTime() / 1000), // timestamp
+      contents: {
+        type: 'block',
+        txList, // transactions
+      },
+    };
+    return newBlock;
   }
 
   // What we do when we get a new block (from ourselves or the network)
   receiveBlock(block) {
     // create a variable to hold the hash of the old block so that we can mine on top of it
+    const { parentHash } = this.blockAttempt;
     // use the receiveBlock() function from the client to check the block and broadcast it to to the network
+    super.receiveBlock(block);
     // update the head of the local state of our blockchain
+    const newHead = this.blockchain.slice(-1)[0];
     // if the block head has changed, mine on top of the new head
-    // - start creating/mining a new block
+    if (getTxHash(newHead) !== parentHash) {
+      // start creating/mining a new block
+      this.blockAttempt = this.createBlock();
+    }
   }
 
   // Start mining
   tick() {
     // for every instance we try to mine (where hashRate determines the amount of computations a GPU or ASIC could process in parrallel)
-    // - if we find a valid block
-    // -- get the valid blockhash
-    // -- log the results to the console
-    // -- create the valid block
-    // -- process the valid block
-    // -- end the loop and keep mining
-    // - if we did not find a block, incriment the nonce and try again
+    for (let i = 0; i < this.hashRate; i++) {
+      // - if we find a valid block
+      if (this.isValidBlockHash(this.blockAttempt)) {
+        // -- get the valid blockhash
+        const validBlockHash = getTxHash(this.blockAttempt);
+        // -- log the results to the console
+        console.log(`blockhash is ${validBlockHash}`);
+        // -- create the valid block
+        this.receiveBlock(this.blockAttempt);
+        // -- process the valid block
+        this.applyBlock(this.blockAttempt);
+        // -- end the loop and keep mining
+        return;
+      }
+      // - if we did not find a block, incriment the nonce and try again
+      this.blockAttempt.nonce++;
+    }
   }
 }
 
